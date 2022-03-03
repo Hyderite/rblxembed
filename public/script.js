c0776a8b4fb133c9276b7c977474fc2f8de1d54b
Hyderite <79314475+Hyderite@users.noreply.github.com>
2022-03-03 15:21:43 +0800

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,16 @@
+document.querySelector("#scroll-to-result").onclick = function() {
+  document.querySelector("#embed-thumbnail").src = document.querySelector("#thumbnail").value;
+  document.querySelector("#game-title").innerHTML = document.querySelector("#gamename").value;
+  if (document.querySelector("#authorlink").value.includes("https://")) {
+    document.querySelector("#username").innerHTML = '<a href="' + document.querySelector("#authorlink").value + '" rel="nofollow noreferrer noopener" target="_blank" id="username">' + document.querySelector("#author").value + '</a>';
+  } else if (document.querySelector("#authorlink").value.includes("http://")) {
+    document.querySelector("#username").innerHTML = '<a href="' + document.querySelector("#authorlink").value + '" rel="nofollow noreferrer noopener" target="_blank" id="username">' + document.querySelector("#author").value + '</a>';
+  } else {
+    document.querySelector("#username").innerHTML = '<a href="' + "https://" + document.querySelector("#authorlink").value + '" rel="nofollow noreferrer noopener" target="_blank" id="username">' + document.querySelector("#author").value + '</a>';
+  };  
+  document.querySelector("#play-button").href = document.querySelector("#gamelink").value;
+  document.querySelector("#code2").innerHTML = '<div class="embed"><div class="embed-container"><img src="' + document.querySelector("#thumbnail").value + '" alt="The link of the thumbnail is broken. Please enter another link." class="embed-thumbnail"><div class="game-info"><p class="game-title">' + document.querySelector("#gamename").value + '</p><p class="game-author">By <a href="' + document.querySelector("#authorlink").value + '" class="username">' + document.querySelector("#author").value + '</a></p><a href="' + document.querySelector("#gamelink").value + '" class="play-button"><span class="play-image"></span></a></div></div></div>';
+  document.querySelector(".error").style.display = "none";
+  document.querySelector("#instructions").style.display = "block";
+  document.querySelector("#embed").style.display = "block";
+}
