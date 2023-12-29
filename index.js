ec995131b3bd03584675e3efb9aa5cf46bd2d301
hyderite <79314475+Hyderite@users.noreply.github.com>
2023-12-29 13:13:19 +0800

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-require("express")().use(require("express").static(__dirname + "/public")).use((_, res) => {
-    res.status(404).sendFile(__dirname + "/page-not-found.html");
-}).listen(8080);
-
-// © 2021 Hyderite. All rights reserved.
